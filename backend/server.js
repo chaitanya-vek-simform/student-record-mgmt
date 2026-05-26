0c2bf42560c8c72605ea997dd55b0c9523a0978f
chaitanya-vek-simform <chaitanya-vek-simform>
2026-05-26 20:27:30 +0530

    fix: change PORT fallback from 5000 to 8080 for Azure App Service

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
